45ed4dd29e791d1e82a41c647d41ce6f8b927b62
ChKhasan <chulliyevhasan1405@mail.ru>
2022-12-06 13:20:44 +0500

    first deploy

---
+++ pages/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- <Tutorial/> -->
+  <div>
+    <h1>Welcome to my site !!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      title: "Welcome to my site",
+      description:
+        "Bu yerdan seo haqida o'zbekcha ma'lumotlar topishingiz mumkin, nafaqat seo va boshqa generativ tizimlar haqida ham",
+    };
+  },
+  mounted() {
+console.log(window.location.host + this.$route.path);
+},
+  head() {
+    return {
+      title: this.title,
+      description: this.description,
+      meta: [
+        {
+          hid: "og:title",
+          name: "og:title",
+          content: this.title,
+        },
+        {
+          hid: "og:description",
+          name: "og:description",
+          content: this.description,
+        },
+        {
+          hid: "og:url",
+          name: "og:url",
+          content: `${window.location.host}${this.$route.path}`,
+        },
+        { hid: "og:type", name: "og:type", content: "shopping" },
+        {
+          hid: "og:keywords",
+          name: "og:keywords",
+          content:
+            "nuxtjs, seo, javascript,seo haqida, development, software,uzbekcha seo ,malumot homepage,khasan,meta tag,meta teg,teg,mega,robots",
+        },
+      ],
+    };
+  },
+};
+</script>
